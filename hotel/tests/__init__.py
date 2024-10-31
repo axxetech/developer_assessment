f2da06e1d330e2f2aef0d7eb79e768455d31f462
Steven <steven@runnr.ai>
2024-10-31 11:34:49 +0100

    api fixtures are always strings, for testing

--- hotel/tests/__init__.py
+++ hotel/tests/__init__.py
@@ -2,14 +2,7 @@ import json
 import os
 
 
-def load_api_fixture(filename: str, as_string=False):
+def load_api_fixture(filename: str) -> str:
     base_dir = os.path.realpath(os.path.dirname(__file__))
-
-    if filename.endswith(".json"):
-        with open(f"{base_dir}/api_fixtures/{filename}") as f:
-            if as_string:
-                return f.read()
-
-            return json.load(f)
-    else:
-        return open(f"{base_dir}/api_fixtures/{filename}").read()
+    with open(f"{base_dir}/api_fixtures/{filename}") as f:
+        return f.read()
